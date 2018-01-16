1e8ac86c389c0125f91233552f247e9f81ebbbc9
Dawid Seredyński <dawid.seredynski@gmail.com>
2018-01-16 17:39:52 +0100

    plane fragments segmentation

--- src/find_features_pc.cpp
+++ src/find_features_pc.cpp
@@ -65,6 +65,7 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 #include <collision_convex_model/collision_convex_model.h>
 
@@ -75,7 +76,7 @@
 class FindFeaturesPC {
 public:
     ros::NodeHandle &nh_;
-    bool point_cloud_processed_;
+//    bool point_cloud_processed_;
     double tolerance_;
 
     tf::TransformListener tf_listener_;
@@ -89,25 +90,30 @@ public:
 
     MarkerPublisher mp_;
 
+//    ros::Subscriber pc_sub_;
+
 public:
 
     void insertCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud){
-        if (point_cloud_processed_) {
-            point_cloud_processed_ = false;
+//        if (point_cloud_processed_) {
+//            point_cloud_processed_ = false;
             pcl::fromROSMsg(*cloud, *pc_);
             pc_stamp_ = cloud->header.stamp;
             pc_frame_id_ = cloud->header.frame_id;
-        }
+//        }
     }
 
     FindFeaturesPC(ros::NodeHandle &n) :
         nh_(n),
         mp_(n),
-        point_cloud_processed_(true),
+//        point_cloud_processed_(true),
         tolerance_(0.04),
         pc_(new PclPointCloud)
     {
+//        pc_sub_ = nh_.subscribe("cloud_in", 5, &FindFeaturesPC::insertCloudCallback, this);
+
         m_pointCloudSub = new message_filters::Subscriber<sensor_msgs::PointCloud2> (nh_, "cloud_in", 5);
+//        m_tfPointCloudSub->registerCallback(boost::bind(&FindFeaturesPC::insertCloudCallback, this, _1));
         m_tfPointCloudSub = new tf::MessageFilter<sensor_msgs::PointCloud2> (*m_pointCloudSub, tf_listener_, "world", 5);
         m_tfPointCloudSub->registerCallback(boost::bind(&FindFeaturesPC::insertCloudCallback, this, _1));
     }
@@ -126,10 +132,23 @@ bool findPlanes(pointcloud_utils_msgs::FindPlanes::Request  &req,
         return false;
     }
 
+    tf::StampedTransform tf_W_C;
+    try {
+        pcf->tf_listener_.lookupTransform("world", pcf->pc_frame_id_, ros::Time(pcf->pc_stamp_.toSec()), tf_W_C);
+    } catch(tf::TransformException& ex){
+        ROS_ERROR_STREAM( "Transform error of sensor data: " << ex.what() << ", quitting callback");
+//        errors++;
+        return false;
+    }
+    geometry_msgs::TransformStamped tfm_W_C;
+    KDL::Frame T_W_C;
+    tf::transformStampedTFToMsg(tf_W_C, tfm_W_C);
+    tf::transformMsgToKDL(tfm_W_C.transform, T_W_C);
+
     ROS_INFO("findPlanes: point cloud has %d points", (int)pcf->pc_->points.size());
 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr pc(new pcl::PointCloud<pcl::PointXYZRGB>(*pcf->pc_));
-
+/*
     pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
     ne.setInputCloud (pc);
 
@@ -194,7 +213,7 @@ bool findPlanes(pointcloud_utils_msgs::FindPlanes::Request  &req,
     }
 
     ROS_INFO("findPlanes: points on planes: %d", counter);
-
+*/
 
 
 
@@ -219,7 +238,7 @@ bool findPlanes(pointcloud_utils_msgs::FindPlanes::Request  &req,
 //  pcl::PCDWriter writer;
 //  writer.write<pcl::PointXYZ> ("table_scene_lms400_downsampled.pcd", *cloud_filtered, false);
 
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+//  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
   // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZRGB> seg;
@@ -242,12 +261,40 @@ bool findPlanes(pointcloud_utils_msgs::FindPlanes::Request  &req,
   {
     // Segment the largest planar component from the remaining cloud
     seg.setInputCloud (cloud_filtered);
-    seg.segment (*inliers, *coefficients);
+
+    pcl::ModelCoefficients coeff;
+    seg.segment (*inliers, coeff);//*coefficients);
     if (inliers->indices.size () == 0)
     {
       std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
       break;
     }
+
+//    double coeff_a = coeff.values[0];
+//    double coeff_b = coeff.values[1];
+//    double coeff_c = coeff.values[2];
+//    double coeff_d = coeff.values[3];
+//    double uu = -coeff_d / (coeff_a*coeff_a + coeff_b*coeff_b + coeff_c*coeff_c);
+//    KDL::Vector orig(uu*coeff_a + uu*coeff_b + uu*coeff_c);
+//    KDL::Vector nz(coeff_a, coeff_b, coeff_c), nx, ny;
+//    nz.Normalize();
+//    if (fabs(nz.z()) > 0.7) {
+//        nx = KDL.Vector(1,0,0);
+//    }
+//    else {
+//        nx = KDL.Vector(0,0,1);
+//    }
+//    ny = nz * nx;
+//    nx = ny * nz;
+//    nx.Normalize();
+//    ny.Normalize();
+//    KDL::Frame fr(KDL::Rotation(nx,ny,nz), orig);
+//ax+by+cz+d=0
+//x=ua
+//y=ub
+//z=uc
+//uaa+ubb+ucc+d=0
+//u=-d/(aa+bb+cc)
 
     // Extract the inliers
     extract.setInputCloud (cloud_filtered);
@@ -256,20 +303,69 @@ bool findPlanes(pointcloud_utils_msgs::FindPlanes::Request  &req,
     extract.filter (*cloud_p);
     std::cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << std::endl;
 
-    int size = cloud_p->points.size();
+/*    int size = cloud_p->points.size();
     double c_r = (double)(rand()%1000) / 1000.0;
     double c_g = (double)(rand()%1000) / 1000.0;
     double c_b = (double)(rand()%1000) / 1000.0;
-//    for (int i = 0; i < 500; i++) {
-//        int ptidx = rand()%size;
     for (int i = 0; i < size; i+=2) {
         int ptidx = i;
-        m_id = pcf->mp_.addSinglePointMarker(m_id, KDL::Vector(cloud_p->points[ptidx].x, cloud_p->points[ptidx].y, cloud_p->points[ptidx].z), c_r, c_g, c_b, 1, 0.01, pcf->pc_frame_id_);
+        m_id = pcf->mp_.addSinglePointMarker(m_id, T_W_C * KDL::Vector(cloud_p->points[ptidx].x, cloud_p->points[ptidx].y, cloud_p->points[ptidx].z), c_r, c_g, c_b, 1, 0.01, "world");//pcf->pc_frame_id_);
     }
+*/
+
+//    for (int i = 0; i < size; i++) {
+//        KDL::Vector pt = fr * KDL::Vector(cloud_p->points[ptidx].x, cloud_p->points[ptidx].y, cloud_p->points[ptidx].z);
+//    }
+
 
 //    std::stringstream ss;
 //    ss << "table_scene_lms400_plane_" << i << ".pcd";
 //    writer.write<pcl::PointXYZ> (ss.str (), *cloud_p, false);
+
+
+
+
+
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+    tree->setInputCloud (cloud_p);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_p);
+    ec.extract (cluster_indices);
+
+//    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+//        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+
+        double c_r = (double)(rand()%1000) / 1000.0;
+        double c_g = (double)(rand()%1000) / 1000.0;
+        double c_b = (double)(rand()%1000) / 1000.0;
+
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit) {
+//            cloud_cluster->points.push_back (cloud_p->points[*pit]); //*
+
+            m_id = pcf->mp_.addSinglePointMarker(m_id, T_W_C * KDL::Vector(cloud_p->points[*pit].x, cloud_p->points[*pit].y, cloud_p->points[*pit].z), c_r, c_g, c_b, 1, 0.01, "world");
+        }
+//        cloud_cluster->width = cloud_cluster->points.size ();
+//        cloud_cluster->height = 1;
+//        cloud_cluster->is_dense = true;
+
+//        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+//        std::stringstream ss;
+//        ss << "cloud_cluster_" << j << ".pcd";
+//        writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //*
+//        j++;
+    }
+
+
+
 
     // Create the filtering object
     extract.setNegative (true);
